957343eb78647f2e8ae5968d5583b22f59beb731
RedCrabVb <cany245@mail.ru>
2021-10-26 11:37:57 +0300

    Add qr code scan

--- app/src/main/java/ru/vivt/applicationmvp/MainActivity.java
+++ app/src/main/java/ru/vivt/applicationmvp/MainActivity.java
@@ -1,9 +1,16 @@
 package ru.vivt.applicationmvp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.zxing.Result;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -15,6 +22,8 @@ import ru.vivt.applicationmvp.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+//    private CodeScanner mCodeScanner;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main2);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
 
     }
 
--- app/src/main/java/ru/vivt/applicationmvp/ui/ScannerQrFragment.java
+++ app/src/main/java/ru/vivt/applicationmvp/ui/ScannerQrFragment.java
@@ -1,19 +1,34 @@
 package ru.vivt.applicationmvp.ui;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
+
+import ru.vivt.applicationmvp.MainActivity;
 import ru.vivt.applicationmvp.R;
+import ru.vivt.applicationmvp.databinding.ActivityMainBinding;
 import ru.vivt.applicationmvp.databinding.FragmentRegestrationBinding;
 import ru.vivt.applicationmvp.databinding.FragmentScannerQrBinding;
 
 public class ScannerQrFragment extends Fragment {
     private FragmentScannerQrBinding binding;
+    private CodeScanner mCodeScanner;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -22,6 +37,37 @@ public class ScannerQrFragment extends Fragment {
         binding = FragmentScannerQrBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        try {
+            if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+                ActivityCompat.requestPermissions(getActivity(), new String[] { Manifest.permission.CAMERA }, 100);
+            }
+            else {
+                Toast.makeText(getActivity(), "Permission already granted", Toast.LENGTH_SHORT).show();
+            }
+
+            if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_DENIED) {
+                CodeScannerView scannerView = binding.scannerView;
+                mCodeScanner = new CodeScanner(getActivity(), scannerView);
+                mCodeScanner.setDecodeCallback(result -> getActivity().runOnUiThread(() -> Toast.makeText(getActivity(), result.getText(), Toast.LENGTH_SHORT).show()));
+                scannerView.setOnClickListener(view -> mCodeScanner.startPreview());
+            } else {
+                throw new RuntimeException();
+            }
+        } catch (Exception | Error e) {
+            getActivity().runOnUiThread(() -> Toast.makeText(getActivity(), "Error, разрешите приложению доступ к камере", Toast.LENGTH_SHORT).show());
+        }
         return root;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mCodeScanner.startPreview();
+    }
+
+    @Override
+    public void onPause() {
+        mCodeScanner.releaseResources();
+        super.onPause();
     }
 }
